f7700e6cd239f3ca742a7d9d15d58a7a1cdd80d0
RuchitaBK <kshirsagarruchita@gmail.com>
2018-06-05 19:24:20 +0530

    Sorting Algorithm_Insertion and Bubble sort

--- Sorting_Algorithm/src/Basic_algorithm.java
+++ Sorting_Algorithm/src/Basic_algorithm.java
@@ -54,7 +54,43 @@ public class Basic_algorithm {
 			}
 	}
 	
+	public void insertionsort()
+	{
+		System.out.println("Enter the No of element you want to insert");
+		int n=sc.nextInt();
+		System.out.print("Accpet input data");
+		int arr_ip[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr_ip[i]=sc.nextInt();
+		}
+		
+		System.out.println("Input  Array");
+		for(int i=0;i<n;i++)
+		{
+			System.out.print(arr_ip[i]+" ");
+		}
+		
+	int temp,position;
+	for(int i=1;i<n;i++)
+	{
+	 temp=arr_ip[i];
+      position=i;
+	      while(arr_ip[position-1]>=temp && position>0)
+	      {
+	    	  arr_ip[position]=arr_ip[position-1];
+	    	  position=position-1;
+	      }
+        arr_ip[position]=temp;
 	
+     }	
+	System.out.println("OUTPUT");
+	for(int i=0;i<n;i++)
+	{
+		System.out.println(arr_ip[i]);
+	}
+		
+	}
 	//============================================================================
 	
 
--- Sorting_Algorithm/src/InitateSort.java
+++ Sorting_Algorithm/src/InitateSort.java
@@ -7,20 +7,21 @@ public class InitateSort {
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
 		// TODO Auto-generated method stub
-		Basic_algorithm bl=new Basic_algorithm();
+		Basic_algorithm b1=new Basic_algorithm();
 		System.out.println("Which SOrting you want perform ");
 		System.out.println("\n 1.Bubble Sort \n 2.insertion sort \n 3. Merge Sort ");
 		int choice =scan.nextInt();
 		switch(choice)
 		{
 		case 1: 
-			   bl.bubblesort();
+			   b1.bubblesort();
 			   break;
 		case 2:
+			b1.insertionsort();
 			 break;
 		}
 
-		bl.bubblesort();
+		
 	}
 
 }
